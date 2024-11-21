f788a04788ead593f28eb8ccee7a79758c3c4754
Song-aff <281218023@qq.com>
2024-11-21 16:38:09 +0000

    fn_test

--- exercises/02_functions/functions5.rs
+++ exercises/02_functions/functions5.rs
@@ -1,6 +1,8 @@
 // TODO: Fix the function body without changing the signature.
 fn square(num: i32) -> i32 {
-    num * num;
+    // num * num
+    // return num.pow(2);
+    return num * num;
 }
 
 fn main() {
