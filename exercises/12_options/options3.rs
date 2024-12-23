1ad8680321c506abcdbc0baa3c44b5b38401bac9
Song-aff <281218023@qq.com>
2024-12-23 12:44:04 +0000

    update

--- exercises/12_options/options3.rs
+++ exercises/12_options/options3.rs
@@ -8,8 +8,15 @@ fn main() {
     let optional_point = Some(Point { x: 100, y: 200 });
 
     // TODO: Fix the compiler error by adding something to this match statement.
+    // 1
+    // match &optional_point {
+    //     Some(p) => println!("Co-ordinates are {},{}", p.x, p.y),
+    //     _ => panic!("No match!"),
+    // }
+
     match optional_point {
-        Some(p) => println!("Co-ordinates are {},{}", p.x, p.y),
+        // 方法2
+        Some(ref p) => println!("Co-ordinates are {},{}", p.x, p.y),
         _ => panic!("No match!"),
     }
 
--- exercises/13_error_handling/errors1.rs
+++ exercises/13_error_handling/errors1.rs
@@ -4,17 +4,21 @@
 // construct to `Option` that can be used to express error conditions. Change
 // the function signature and body to return `Result<String, String>` instead
 // of `Option<String>`.
-fn generate_nametag_text(name: String) -> Option<String> {
+fn generate_nametag_text(name: String) -> Result<String, String> {
     if name.is_empty() {
         // Empty names aren't allowed
-        None
+        // None
+        Err("Empty names aren't allowed".to_string())
     } else {
-        Some(format!("Hi! My name is {name}"))
+        Ok(format!("Hi! My name is {name}"))
     }
 }
 
 fn main() {
     // You can optionally experiment here.
+    // let a = Some(1);
+    // let b = a.as_ref();
+    // let c = a.as_deref();
 }
 
 #[cfg(test)]
